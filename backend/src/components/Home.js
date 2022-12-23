94a6821aab23fc068b9bb75df9f74afc4f05dee0
prasert rueangmanee <kengzer5@hotmail.com>
2022-12-23 16:24:17 +0700

    add

---
+++ backend/src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Page from './Page'
+
+function Home() {
+    
+  return (
+    <Page name="Keng"/>
+  )
+}
+
+export default Home
---
+++ backend/src/components/Page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Page(props) {
+    const name = props.name
+  return (
+    <div> Hello Component {name}</div>
+
+  )
+}
+
+export default Page;
